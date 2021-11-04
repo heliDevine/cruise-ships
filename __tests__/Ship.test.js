7e23e6a2de4fb8d25af79f8870438cff157d6d57
Heli Devine <heli.devine@gmail.com>
2021-11-04 15:15:31 +0000

    first test and Ship object created

---
+++ __tests__/Ship.test.js
@@ -0,0 +1,9 @@
+/* globals describe it expect */
+const Ship = require('../src/Ship.js');
+
+describe ('Ship', () => {
+    it ('can be instantiated',() => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+});
+
