52193718ff839969bc0fb307bb1e93b6b24e5863
Heli Devine <heli.devine@gmail.com>
2021-11-11 17:33:09 +0000

    added addShip and removeShip tests and methods

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -9,4 +9,20 @@ describe ('Port', () => {
         const port = new Port('Ningbo');
         expect(port.name).toBe('Ningbo');
       });
+    it('can add a ship', () => {
+        const port = new Port('Dover');
+        const ship ={};
+        port.addShip(ship);
+        expect(port.ships).toContain(ship);
+        });
+    it ('can remove a ship', () => {
+        const port = new Port ('Dover');
+        const titanic = {};
+        const olympic = {};
+        port.addShip(titanic);
+        port.addShip(olympic);
+        port.removeShip(titanic);
+        expect(port.ships).toEqual([titanic]);
+    });
+
 });
