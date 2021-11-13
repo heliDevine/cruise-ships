c5abe4215813775531e723533f6533cb1056662f
Heli Devine <heli.devine@gmail.com>
2021-11-13 15:54:28 +0000

    isolating Port tests with dummies

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -12,14 +12,14 @@ describe("Port", () => {
 		});
 
 		it("can add a ship", () => {
-			const ship = {};
+			const ship = jest.fn();
 			port.addShip(ship);
 			expect(port.ships).toContain(ship);
 		});
 
 		it("can remove a ship", () => {
-			const titanic = {};
-			const olympic = {};
+			const titanic = jest.fn();
+			const olympic = jest.fn();
 			port.addShip(titanic);
 			port.addShip(olympic);
 			port.removeShip(titanic);
